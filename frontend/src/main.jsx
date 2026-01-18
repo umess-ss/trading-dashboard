e928558aa052e694ab31e37da67ad6811ef677fa
Umesh <ums.rbc07@gmail.com>
2026-01-18 16:29:27 +0545

    trigger css issue in ci

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './app.css'
+import './App.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
