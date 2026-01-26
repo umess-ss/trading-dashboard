9faf6257244f6d0328bec0bd4745d6588c2d5c8a
Umesh <ums.rbc07@gmail.com>
2026-01-26 23:40:57 +0545

    feat(order): add validation in serializer

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -7,3 +7,25 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['id','asset','asset_symbol','side','price','quantity','status','timestamp']
         read_only_fields = ['status', 'timestamp']
+
+    def validate(self,data):
+        user = self.context['request'].user
+        profile = user.profile
+
+
+        total_cost = data['price'] * data['quantity']
+
+        if data['side'] == 'BUY':
+            if profile.usdt_balance < total_cost:
+                raise serializers.ValidationError("Insufficient USDT balance.")
+            
+        elif data['side'] == 'SELL':
+            asset_symbol = data['asset'].symbol.lower()
+            balance_field = f"{asset_symbol}_balance"
+            user_crypto_balance = getattr(profile, balance_field, 0)
+
+
+            if user_crypto_balance < data['quantity']:
+                raise serializers.ValidationError("Insufficient {asset_symbol.upper()} balance")
+            
+        return data
